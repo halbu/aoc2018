f1aee5619bdd0c0c96dba019fe9c85aa62730d11
halbu <fstream.c@gmail.com>
2018-12-02 16:13:34 +0000

    Streamline a bit and remove unnecessary flags

--- aoc2018-day2/aoc2018-day2-1.py
+++ aoc2018-day2/aoc2018-day2-1.py
@@ -1,24 +1,16 @@
-data = open('./aoc2018-day2.data', "r")
+doubles, triples = 0, 0
 
-doubles = 0
-triples = 0
-
-for line in data:
+for line in open('./aoc2018-day2.data', "r"):
   chars = {}
   for c in line:
     if c not in chars:
       chars[c] = 1
     else:
       chars[c] = chars[c] + 1
-  
-  logged_a_double = False
-  logged_a_triple = False
-  for key in chars:
-    if chars[key] == 2 and not logged_a_double:
-      doubles = doubles + 1
-      logged_a_double = True
-    if chars[key] == 3 and not logged_a_triple:
-      triples = triples + 1
-      logged_a_triple = True
+
+  if 2 in chars.values():
+    doubles = doubles + 1
+  if 3 in chars.values():
+    triples = triples + 1
 
 print('Checksum (doubles * triples) = ' + str(doubles * triples))
