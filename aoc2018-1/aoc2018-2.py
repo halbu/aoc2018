7838e19a04dd6d125408dd38b15a9b24fed6e727
halbu <fstream.c@gmail.com>
2018-12-02 01:42:50 +0000

    Golfed a bit

--- aoc2018-1/aoc2018-2.py
+++ aoc2018-1/aoc2018-2.py
@@ -1,12 +1,10 @@
-import sys
 count = 0
 appearances = {}
-file = open('./aoc2018-1.data', "r")
 
 while(True):
-  file.seek(0)
-  for line in file:
-    if (count = count + int(line)) in appearances.keys():
+  for line in open('./aoc2018-1.data', "r"):
+    count = count + int(line)
+    if count in appearances.keys():
       print('First repeated frequency: ' + str(count))
-      sys.exit()
+      exit()
     appearances[count] = 1
