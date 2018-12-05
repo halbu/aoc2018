afd63d07f66bfa4305d5ff54d9d95566b5bafb3f
halbu <fstream.c@gmail.com>
2018-12-05 21:52:32 +0000

    Streamlined react function

--- aoc2018-day5/aoc2018-day5-2.py
+++ aoc2018-day5/aoc2018-day5-2.py
@@ -16,10 +16,7 @@ def react(polymer):
   out = ""
   i = 0
   while i < len(polymer):
-    if i == len(polymer)-1:
-      out = out + polymer[i]
-      i += 1
-    elif (abs(ord(polymer[i]) - ord(polymer[i+1])) == 32):
+    if i < len(polymer)-1 and (abs(ord(polymer[i]) - ord(polymer[i+1])) == 32):
       i += 2
     else:
       out = out + polymer[i]
