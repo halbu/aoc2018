fce07a7e6725138337bc128e585c54c193d29b1d
halbu <fstream.c@gmail.com>
2018-12-08 13:52:52 +0000

    Shorten a bit

--- aoc2018-day8/aoc2018-day8-2.py
+++ aoc2018-day8/aoc2018-day8-2.py
@@ -1,27 +1,20 @@
-dlist = ([str.rstrip(l) for l in open('./aoc2018-day8.data', "r")])[0].split(' ')
-metadata = []
+dlist = [int(k) for k in ([l for l in open('./aoc2018-day8.data', "r")])[0].split(' ')]
 index = 0
-
-def main():
-  print('Solution: ' + str(process_node()))
 
 def process_node():
   global index
-
-  num_children, num_metadatas = int(dlist[index]), int(dlist[index + 1])
+  num_children, num_metadatas = dlist[index], dlist[index + 1]
   index += 2
-
   child_values = [process_node() for k in range(0, num_children)]
 
   value = 0
   for j in range(0, num_metadatas):
     if len(child_values) == 0:
-        value += int(dlist[index])
-    elif int(dlist[index]) > 0 and int(dlist[index]) <= len(child_values):
-        value += child_values[int(dlist[index]) - 1]
+        value += dlist[index]
+    elif dlist[index] > 0 and dlist[index] <= len(child_values):
+        value += child_values[dlist[index] - 1]
     index += 1
 
   return value
 
-if __name__ == '__main__':
-  main()
+print('Solution: ' + str(process_node()))
