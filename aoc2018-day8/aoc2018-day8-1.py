9b03714e06865e9a1ff249fb8ddea4905fc69e14
halbu <fstream.c@gmail.com>
2018-12-08 14:02:11 +0000

    Refactor & cleanup; read values from data as ints rather than casting them everywhere

--- aoc2018-day8/aoc2018-day8-1.py
+++ aoc2018-day8/aoc2018-day8-1.py
@@ -1,28 +1,18 @@
-
-data = ([str.rstrip(l) for l in open('./aoc2018-day8.data', "r")])[0]
-dlist = [k for k in data.split(' ')]
+dlist = [int(k) for k in ([l for l in open('./aoc2018-day8.data', "r")])[0].split(' ')]
 metadata = []
 index = 0
 
-def main():
-  while index < len(dlist):
-    process_node()
-  
-  print('Solution: ' + str(sum(metadata)))
-
 def process_node():
   global index
-
-  num_children = int(dlist[index])
-  num_metadatas = int(dlist[index + 1])
+  num_children, num_metadatas = dlist[index], dlist[index + 1]
   index += 2
 
   for i in range(0, num_children):
     process_node()
   
   for j in range(0, num_metadatas):
-    metadata.append(int(dlist[index]))
+    metadata.append(dlist[index])
     index += 1
-
-if __name__ == '__main__':
-  main()
+    
+process_node()
+print('Solution: ' + str(sum(metadata)))
