6ea8046c31e24dae8430a8e839391eae3cf27b98
halbu <fstream.c@gmail.com>
2018-12-15 15:20:18 +0000

    Refactor to use cart list

--- aoc2018-day13/aoc2018-day13-1.py
+++ aoc2018-day13/aoc2018-day13-1.py
@@ -1,10 +1,10 @@
+import random
 data = [l.rstrip('\n') for l in open('./aoc2018-day13.data', "r")]
 h, w = len(data), len(data[0])
-
 track_grid = [['.' for _ in range(h)] for _ in range(w)]
-cart_grid = [[{} for _ in range(h)] for _ in range(w)]
-new_cart_grid = [[{} for _ in range(h)] for _ in range(w)]
+carts = []
 
+# Translation maps
 dirs = {"^": "U", "v": "D", "<": "L", ">": "R"}
 turn_order = {"L": "S", "S": "R", "R": "L"}
 left_turn = { "U": "L", "L": "D", "D": "R", "R": "U"}
@@ -19,23 +19,19 @@ class Cart():
     self.y = y
     self.d = d
     self.t = "L"
+    self.uid = random.uniform(0, 1)
 
   def act(self):
-    global cart_grid
-    global new_cart_grid
-    cart_grid[self.x][self.y] = {}
-
     self.x += deltas[self.d][0]
     self.y += deltas[self.d][1]
 
-    # test for crash
-    if new_cart_grid[self.x][self.y] != {} or cart_grid[self.x][self.y] != {}:
-      print('Crash!!! ' + str(self.x) + ', ' + str(self.y))
-      exit()
-      
-    new_cart_grid[self.x][self.y] = self
+    # Test for crash
+    for c in carts:
+      if c.uid != self.uid and c.x == self.x and c.y == self.y:
+        print('Crash!!! ' + str(self.x) + ', ' + str(self.y))
+        exit()
 
-    # consider new square
+    # Determine new direction
     gs = track_grid[self.x][self.y]
     if gs in ["\\", "/"]:
       self.d = backslash[self.d] if gs == "\\" else forwardslash[self.d]
@@ -49,28 +45,23 @@ class Cart():
 def main():
   setup()
   while(True):
-    tick()
+    tick()  
 
 def setup():
   global track_grid
-  global cart_grid
   for i in range(0, w):
     for j in range(0, h):
       if data[j][i] in ["<", ">", "^", "v"]:
-        cart_grid[i][j] = Cart(i, j, dirs[data[j][i]])
+        carts.append(Cart(i, j, dirs[data[j][i]]))
         track_grid[i][j] = "-" if data[j][i] in ["<", ">"] else "|"
       else:
         track_grid[i][j] = data[j][i]
 
 def tick():
-  global cart_grid
-  global new_cart_grid
-  new_cart_grid = [[{} for _ in range(h)] for _ in range(w)]
-  for i in range(0, w):
-    for j in range(0, h):
-      if cart_grid[i][j] != {}:
-        cart_grid[i][j].act()
-  cart_grid = new_cart_grid
+  global carts
+  carts = sorted(carts, key=lambda c: c.y * w + c.x)
+  for c in carts:
+    c.act()
 
 if __name__ == '__main__':
   main()
