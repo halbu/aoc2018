a9247c42eeac0134ce7af039993cde6c8f90e58a
halbu <fstream.c@gmail.com>
2019-01-04 17:01:17 +0000

    Admission of failure

--- aoc2018-day15/aoc2018-day15-1.py
+++ aoc2018-day15/aoc2018-day15-1.py
@@ -1,13 +1,27 @@
-data = list([str.rstrip(l) for l in open('./aoc2018-day15.data', "r")])
-world = [["." for _ in range(len(data[0]))] for _ in range(len(data))]
+data = list([str.rstrip(l) for l in open('./test2.data', "r")])
+w, h = len(data[0]), len(data)
+world = [['.' for _ in range(h)] for _ in range(w)]
 actors = []
-dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+def main():
+  for i in range(w):
+    for j in range(h):
+      c = data[j][i]
+      world[i][j] = '#' if c == '#' else '.'
+      if c in ['G', 'E']:
+        actors.append(Actor(c, i, j))
+  print_world()
+
+def print_world():
+  for j in range(h):
+    output = ''
+    for i in range(w):
+      output += world[i][j]
+    print(output)
 
 class Actor():
   def __init__(self, c, x, y):
-    self.x = x
-    self.y = y
-    self.c = c
+    self.c, self.x, self.y = c, x, y
     self.hp = 200
     self.ap = 3
     self.acted_this_turn = False
@@ -18,46 +32,29 @@ class Actor():
 
   def untag_map(self):
     world[self.x][self.y] = '.'
-  
-  def find_target(self):
-    targets = [i for i in actors if i.c != self.c]
-    paths = []
-    for t in targets:
-      i = 1
-      paths.append(self.find_path(t))
 
-  def find_path(self, t):
-    openlist = [Node(self.x, self.y, 0)]
+    # # flood fill from the goal point
+    # dmap = []
+    # for i in range(w):
+    #   for j in range(h):
+    #     if i == target['x'] and j == target['y']:
+    #       dmap.append(0)
+    #     elif world[i][j] == '.':
+    #       dmap.append(99999) # arbitrary high number for passable tiles
+    #     else:
+    #       dmap.append(100000) # arbitrary number assigned to impassable tiles
 
-    while(openlist):
-      cnode = openlist.pop(0)
-      # hmm
+    # mutated = True
+    # while mutated:
+    #   mutated_this_iteration = False
+    #   for i in range(len(dmap)):
+    #     if i == 100000:
+    #       continue # skip tiles with our impassable integer in them
+    #     elif get_lowest_neighbour(i) + 1 < dmap[i]:
+    #       dmap[i] = get_lowest_neighbour(i) + 1
+    #       mutated_this_iteration = True
+    #   mutated = mutated_this_iteration
 
-  def report(self):
-    print(vars(self))
-
-class Node():
-  def __init__(self, x, y, s):
-    self.x = x
-    self.y = y
-    self.s = s
-
-def main():
-  setup()
-  for l in world:
-    print(''.join(l))
-
-def setup():
-  global world
-  global actors
-  for i, l in enumerate(data):
-    for j, c in enumerate(l):
-      world[i][j] = '#' if c == '#' else '.'
-      if c in ['E', 'G']:
-        actors.append(Actor(c, i, j))
-
-
-
-
+    
 if __name__ == '__main__':
   main()
