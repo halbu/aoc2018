b0435ef78d2d7d83fabcd94821e4ad3c0b149a9d
halbu <fstream.c@gmail.com>
2019-02-21 23:54:15 +0000

    Tidying

--- aoc2018-day21/aoc2018-day21-1.py
+++ aoc2018-day21/aoc2018-day21-1.py
@@ -16,12 +16,6 @@ def main():
 
   print('Solution: ' + str(calculate_total_risk()))
 
-# The geologic index can be determined using the first rule that applies from the list below:
-# The region at 0,0 (the mouth of the cave) has a geologic index of 0.
-# The region at the coordinates of the target has a geologic index of 0.
-# If the region's Y coordinate is 0, the geologic index is its X coordinate times 16807.
-# If the region's X coordinate is 0, the geologic index is its Y coordinate times 48271.
-# Otherwise, the region's geologic index is the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
 def get_geological_index(x, y):
   if x == 0 and y == 0:
     return 0
@@ -34,7 +28,6 @@ def get_geological_index(x, y):
   else:
     return erosion_grid[x - 1][y] * erosion_grid[x][y - 1]
 
-# A region's erosion level is its geologic index plus the cave system's depth, all modulo 20183. Then:
 def get_erosion_level(geologic_index):
   return (geologic_index + depth) % 20183
 
